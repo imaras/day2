be2758a4c30a06eef6bea9350c2cdc94af9c4518
marasi <ivan.maras@avl.com>
2018-11-13 22:42:12 +0100

    day2 - exercise 02 - klasa sa dinamičkom alokacijom

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,82 @@
+#include <stdexcept>
 #include "array.h"
+
+array::array()
+{
+	p = nullptr;
+	n = 0;
+}
+
+array::array(int size, double value)
+{
+	p = new double[size];
+	for (int i = 0; i < size; ++i)
+	{
+		p[i] = value;
+	}
+	n = size;
+}
+
+void array::copy_impl(const array& other)
+{
+	p = new double[other.n];
+	for (int i = 0; i < other.n; i++)
+	{
+		p[i] = other.p[i];
+	}
+	n = other.n;
+}
+
+array::array(const array& other)
+{
+	if (this != &other)
+	{
+		copy_impl(other);
+	}
+}
+
+array::array(array&& other)
+{
+	p = other.p;
+	n = other.n;
+
+	other.p = nullptr;
+	other.n = 0;
+}
+
+array::~array()
+{
+	delete p;
+	p = nullptr;
+	n = 0;
+}
+
+int array::size() const
+{
+	return n;
+}
+
+
+void check_index_range(int index, int min, int max)
+{
+	if (index < min || index > max)
+	{
+		throw std::out_of_range("index is out of range");
+	}
+}
+
+double array::at(int index) const
+{
+	check_index_range(index, 0, n - 1);
+	return p[index];
+}
+
+array& array::operator=(const array& other)
+{
+	if (this != &other)
+	{
+		delete p;
+		copy_impl(other);
+	}
+	return *this;
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -1,7 +1,17 @@
 // do not use standard container as member or base class
 class array
 {
+private:
 	double* p;
 	int n;
+	void copy_impl(const array& other);
 public:
+	array();
+	array(int size, double value);
+	array(const array& other);
+	array(array&& other);
+	~array();
+	int size() const;
+	double at(int index) const;
+	array& operator=(const array& other);
 };
